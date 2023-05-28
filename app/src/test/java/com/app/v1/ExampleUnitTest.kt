124a6564f563e6ff4e43a0ce386a4281277373d2
swk0826 <swk0826@wavve.com>
2023-05-28 15:20:34 +0900

    [wookyo] Test progect git commit

---
+++ app/src/test/java/com/app/v1/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package com.app.v1
+
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+
+}
